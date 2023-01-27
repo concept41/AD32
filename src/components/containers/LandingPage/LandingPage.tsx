c9f121c32899c0285f4a59e030c7c49679953c1a
fongelias <fongelias@gmail.com>
2023-01-27 16:18:04 -0500

    added landing page

---
+++ src/components/containers/LandingPage/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FlexBox } from "components/presentational/FlexBox/FlexBox";
+
+export const LandingPagePath = "/";
+
+export const LandingPage = () => {
+  return (
+    <div className='LandingPage'>
+      <FlexBox>
+        <span>Ad</span>
+      </FlexBox>
+    </div>
+  );
+}
